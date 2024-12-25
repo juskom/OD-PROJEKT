f9da8d078921234f81e6b64853ebf03d1b485377
Justyna <justyna.komza@interia.pl>
2024-12-25 21:08:30 +0100

    pliki conf, docker, modele dla bazy

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from sqlalchemy import func
+
+from . import db
+from flask import Flask, render_template, request, make_response, redirect
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import markdown
+from collections import deque
+from passlib.hash import sha256_crypt
+import sqlite3
+import bcrypt
+
+db = SQLAlchemy()
+
+
+# Model użytkownika
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)  # Hashed password
+    two_factor_secret = db.Column(db.String(100))  # Secret for TOTP
+    last_failed_login = db.Column(db.DateTime, nullable=True)
+    last_login = db.Column(db.DateTime, default=func.now())
+    failed_login_attempts = db.Column(db.Integer, default=0)
+
+    # Relacja do notatek
+    notes = db.relationship('Note', backref='author', lazy=True)
+    shared_notes = db.relationship('ConnectorNote', backref='user', lazy=True)
+
+
+# Model notatki
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    is_public = db.Column(db.Boolean, default=False)
+    is_encrypted = db.Column(db.Boolean, default=False)
+    encryption_key = db.Column(db.String(200), nullable=True)
+
+    # Relacja z użytkownikami
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+# Model dla połączeń notatek z użytkownikami (udostępnianie notatek)
+class ConnectorNote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    noteID = db.Column(db.Integer, db.ForeignKey('note.id'), nullable=False)
